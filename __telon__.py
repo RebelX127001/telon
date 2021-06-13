a8bf42d045bd79560d681082100fe9fd8350ca77
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-13 06:32:42 +0100

    Update __telon__.py

--- __telon__.py
+++ __telon__.py
@@ -266,7 +266,7 @@ def mainActivity():
         global line
         line = "-" * 70
         global abspath 
-        abspath = gd.path.join ("C://", "virtualenvironment", "TeLon")
+        abspath = gd.path.join ("C://", "virtualenvironment")
         gd.system("cd " + abspath)
         #disC = sp.check_output("zaglarh disconnect")
         indiv.clr()
